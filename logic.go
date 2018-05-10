5f780413223871c7dcf0bd7d42880d9fd6c51abf
Applejam <924904427@qq.com>
2018-05-10 09:57:31 +0800

    New Gui with lxn/walk

---
+++ logic.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
+
+//混凝土轴心抗压强度标准值
+var gfcks []float64 = []float64{16.7, 20.1, 23.4, 26.8}
+
+//混凝土轴心抗压强度设计值
+var gfc []float64 = []float64{11.9, 14.3, 16.7, 19.1}
+
+//混凝土轴心抗拉强度设计值
+var gft []float64 = []float64{1.27, 1.43, 1.57, 1.71}
+
+//混凝土弹性模量
+var gec []float64 = []float64{2.8e+4, 3.0e+4, 3.15e+4, 3.25e+4}
+
+var fy float64 = 360
+var es float64 = 2e5
+
+//计算二元一次方程
+func CalQEquations(xa, xb, xc float64) (xval []float64, err error) {
+	b := math.Pow(xb, 2) - 4*xa*xc
+	if b < 0 {
+		err = errors.New("二次方程无合理解！终止。")
+		return
+	}
+	xval = []float64{(-xb + math.Sqrt(b)) / 2 / xa, (-xb - math.Sqrt(b)) / 2 / xa}
+	return
+}
+
+func userjoin(args ...string) string {
+	return strings.Join(args, " ")
+}
+
+func (con *Cons) Calc() (outstring string) {
+
+	fc := gfc[con.DegreeId]
+	h0 := con.Height - con.As0
+	xa := fc * con.Width / 2
+	xb := -fc * con.Width * h0
+	scale, _ := strconv.ParseFloat(con.Scale, 64)
+	xc := con.Moment * 1e+6 * scale
+	fcuk := gfcks[con.DegreeId] * 1.5
+	ecu := 0.0033 - (fcuk-50)*1e-5
+	if ecu > 0.0033 {
+		ecu = 0.0033
+	}
+	b1 := 0.8
+	eb := b1 / (1 + fy/ecu/es)
+	var x float64
+	xslice, err := CalQEquations(xa, xb, xc)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(0)
+
+	}
+	for _, v := range xslice {
+		if v > 0 && v < eb*h0 {
+			x = v
+			outstring = fmt.Sprintf("现在受压区高度为%v\n", strconv.FormatFloat(x, 'f', 2, 64))
+		}
+	}
+	as_value := fc * con.Width * x / fy
+	p0 := math.Max(45*gft[con.DegreeId]/fy, 0.2)
+	if x > eb*h0 {
+		outstring = userjoin(outstring, "X超过最大受压区高度，增大截面\n")
+	} else {
+		outstring = userjoin(outstring, fmt.Sprintf("构造配筋As为%v\n", strconv.FormatFloat(p0/100*con.Width*con.Height, 'f', 2, 64)))
+		outstring = userjoin(outstring, fmt.Sprintf("配筋面积As为%v\n", strconv.FormatFloat(as_value, 'f', 2, 64)))
+	}
+	return
+}
---
+++ main.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"github.com/lxn/walk"
+	. "github.com/lxn/walk/declarative"
+)
+
+type Cons struct {
+	Height   float64
+	Width    float64
+	As0      float64
+	DegreeId int
+	Scale    string
+	Moment   float64
+	Shear    float64
+	Torque   float64
+}
+type Degree struct {
+	Id  int
+	Val string
+}
+
+func Value() []*Degree {
+	return []*Degree{
+		{0, "C25"},
+		{1, "C30"},
+		{2, "C35"},
+		{3, "C40"},
+	}
+}
+
+var concrete *Cons = new(Cons)
+
+func main() {
+	// walk.FocusEffect, _ = walk.NewBorderGlowEffect(walk.RGB(0, 63, 255))
+	walk.InteractionEffect, _ = walk.NewDropShadowEffect(walk.RGB(63, 63, 63))
+	var db *walk.DataBinder
+	var outTe *walk.TextEdit
+	ico, _ := walk.NewIconFromFile(".\\3dEdgeON.ico")
+	MainWindow{
+		Title:   "Concrete Calc",
+		Icon:    ico,
+		Layout:  VBox{},
+		MinSize: Size{400, 600},
+		DataBinder: DataBinder{
+			AssignTo:   &db,
+			DataSource: concrete,
+			AutoSubmit: true,
+		},
+		Children: []Widget{
+			Composite{
+				Layout: Grid{Columns: 2, Spacing: 2},
+				Children: []Widget{
+					Label{Text: "Width(mm):"},
+					NumberEdit{Value: Bind("Width")},
+					Label{Text: "Height(mm):"},
+					NumberEdit{Value: Bind("Height")},
+					Label{Text: "Moment(kN·m):"},
+					NumberEdit{Value: Bind("Moment")},
+					Label{Text: "Shear(kN):"},
+					NumberEdit{Value: Bind("Shear")},
+					Label{Text: "Torque(kN·m):"},
+					NumberEdit{Value: Bind("Torque")},
+					Label{Text: "Degree:"},
+					ComboBox{
+						Value:         Bind("DegreeId"),
+						CurrentIndex:  1,
+						DisplayMember: "Val",
+						BindingMember: "Id",
+						Model:         Value(),
+					},
+					Label{Text: "Scale:"},
+					ComboBox{
+						Editable:     true,
+						Value:        Bind("Scale"),
+						CurrentIndex: 1,
+						Model:        []string{"1.1", "1.25", "1.3", "1.35"},
+					},
+					Label{Text: "As0:"},
+					NumberEdit{
+						Suffix: " mm",
+						Value:  Bind("As0"),
+					},
+				},
+			},
+			VSeparator{},
+			Composite{
+				Layout: Grid{Columns: 2},
+				Children: []Widget{
+					Label{Text: "Resule:"},
+					HSpacer{},
+				},
+			},
+			TextEdit{
+				AssignTo: &outTe,
+				ReadOnly: true,
+			},
+			Composite{
+				Layout: Grid{Columns: 2},
+				Children: []Widget{
+					HSpacer{},
+					PushButton{
+						Text: "Calc",
+						OnClicked: func() {
+							outTe.SetText(concrete.Calc())
+						},
+					},
+				},
+			},
+		},
+	}.Run()
+}
